015c076b8f61034ee8cc3380727dfab27a23619c
MA-Alumno <MA-Alumno@duoc.acad>
2026-03-11 20:02:23 -0300

    Se actualizo el codigo del archivo 'provider.tf' y se cambio el valor de la linea 61 del archivo 'vpc.tf' debido a un error.

--- EA1/ACT1.1/vpc.tf
+++ EA1/ACT1.1/vpc.tf
@@ -58,7 +58,7 @@ resource "aws_subnet" "subnet_privada_2" {
 
 # Crear un NAT Gateway
 resource "aws_eip" "nat_eip" {
-  domain = "vpc"
+  domain = "vpc231313"
   tags = {
     Name = "nat-eip"
   }
