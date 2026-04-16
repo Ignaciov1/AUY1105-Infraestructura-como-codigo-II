3c5a26d99fe9d5179d0b8728465e97c573594c7e
Ignaciov1 <ig.sariego@duocuc.cl>
2026-04-15 20:16:17 -0400

    Actividad autonoma

--- EA2/ACT2.1/ec2_module/outputs.tf
+++ EA2/ACT2.1/ec2_module/outputs.tf
@@ -1,14 +1,25 @@
-output "instance_id" {
-  description = "ID de la instancia EC2"
-  value       = aws_instance.mi_ec2.id
-}
-
-output "public_ip" {
-  description = "IP pública de la instancia EC2"
+output "instancia_publica_1_ip" {
+  description = "IP publica de la primera instancia"
   value       = aws_instance.mi_ec2.public_ip
 }
 
-output "security_group_id" {
-  description = "ID del grupo de seguridad"
-  value       = aws_security_group.ssh_access.id
+output "instancia_publica_2_ip" {
+  description = "IP publica de la instancia extra"
+  value       = aws_instance.mi_ec2_pub_2.public_ip
+}
+
+output "instancia_privada_ip" {
+  description = "IP privada de la instancia en subred privada"
+  value       = aws_instance.mi_ec2_privada.private_ip
+}
+
+# Aquí estaba el error, ahora apunta a 'sg_publico'
+output "security_group_publico_id" {
+  description = "ID del Security Group publico"
+  value       = aws_security_group.sg_publico.id
+}
+
+output "security_group_privado_id" {
+  description = "ID del Security Group privado"
+  value       = aws_security_group.sg_privado.id
 }
