fd9295a79d26d239cf8c407a99e1cdff9c13a877
Ignaciov1 <ig.sariego@duocuc.cl>
2026-05-06 20:34:16 -0400

    fix: corregir descripcion de variable

--- EA2/ACT2.5/variables.tf
+++ EA2/ACT2.5/variables.tf
@@ -1,39 +1,39 @@
 variable "key_name" {
-  description = "Nombre de la llave SSH"
+  description = "Nombre de la llave SSH."
   type        = string
 }
 
 variable "instance_name" {
-  description = "Nombre de la instancia EC2"
+  description = "Nombre para la instancia EC2."
   type        = string
 }
 
 variable "vpc_name" {
-  description = "Nombre de la VPC"
+  description = "Nombre para la VPC."
   type        = string
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block para la VPC"
+  description = "CIDR block para la VPC."
   type        = string
 }
 
 variable "subnet_publica_1_cidr" {
-  description = "CIDR block para la Subnet Pública 1"
+  description = "CIDR block para la Subnet Pública 1."
   type        = string
 }
 
 variable "subnet_publica_2_cidr" {
-  description = "CIDR block para la Subnet Pública 2"
+  description = "CIDR block para la Subnet Pública 2."
   type        = string
 }
 
 variable "subnet_privada_1_cidr" {
-  description = "CIDR block para la Subnet Privada 1"
+  description = "CIDR block para la Subnet Privada 1."
   type        = string
 }
 
 variable "subnet_privada_2_cidr" {
-  description = "CIDR block para la Subnet Privada 2"
+  description = "CIDR block para la Subnet Privada 2."
   type        = string
 }
